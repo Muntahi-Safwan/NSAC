98e87dedd479abe39b2f53e0cacff8383a83d64a
Muntahi-Safwan <muntahi1212@gmail.com>
2025-10-03 16:00:13 +0600

    feat: Add How We Built It page and update Navbar and Routes for navigation

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'https://airowatch.vercel.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
